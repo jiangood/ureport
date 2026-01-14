d4991e26f3c3dd26c8b285a0fff2e5faa94faf7d
jiangood <410518072@qq.com>
2026-01-14 16:42:00 +0800

    chore(deps): 升级 antlr4-runtime 依赖版本
    - 将 antlr4-runtime 从 4.5.3 版本升级到 4.13.0 版本
    - 更新了 pom.xml 中的依赖版本配置
    - 同步更新了所有相关的 DSL 解析器类中的版本检查代码
    - 确保运行时元数据版本验证与新依赖版本匹配

--- ureport-core/src/main/java/com/bstek/ureport/dsl/ReportLexer.java
+++ ureport-core/src/main/java/com/bstek/ureport/dsl/ReportLexer.java
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class ReportLexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.5.3", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.13.0", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
